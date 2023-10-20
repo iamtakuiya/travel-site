6a63e6024ac3de9adf20e75a66d2afd4c7fe1b56
Takuiya-dev <example@gmail.com>
2023-10-20 20:48:28 +0900

    Updated cloud funtion to check password

--- cloud-functions/secrect-area.js
+++ cloud-functions/secrect-area.js
@@ -1,6 +1,25 @@
 exports.handler = function (event, context, callback) {
-	callback(null, {
-		statusCode: 200,
-		body: 'Welcome to the super secret area'
-	});
+	const secretContent = `
+		<h3>Welcome To tThe Secret Area</h3>
+		<p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two is equal four.</p>
+	`;
+
+	let body;
+
+	if (event.body) {
+		body = JSON.parse(event.body);
+	} else {
+		body = {};
+	}
+
+	if (body.password == 'javascript') {
+		callback(null, {
+			statusCode: 200,
+			body: 'Welcome to the super secret area'
+		});
+	} else {
+		callback(null, {
+			statusCode: 401
+		});
+	}
 };
