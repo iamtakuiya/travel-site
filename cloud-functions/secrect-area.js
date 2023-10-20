086269bd155e525229a4ec4caa0704cd3a511070
Takuiya-dev <example@gmail.com>
2023-10-20 20:57:45 +0900

    Modified secret code

--- cloud-functions/secrect-area.js
+++ cloud-functions/secrect-area.js
@@ -1,8 +1,8 @@
 exports.handler = function (event, context, callback) {
 	const secretContent = `
-		<h3>Welcome To tThe Secret Area</h3>
-		<p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two is equal four.</p>
-	`;
+  <h3>Welcome To The Secret Area</h3>
+  <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+  `;
 
 	let body;
 
@@ -15,7 +15,7 @@ exports.handler = function (event, context, callback) {
 	if (body.password == 'javascript') {
 		callback(null, {
 			statusCode: 200,
-			body: 'Welcome to the super secret area'
+			body: secretContent
 		});
 	} else {
 		callback(null, {
